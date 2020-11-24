69bbcfebfc98daaa1fd668fc3889ecf6a23e6b87
Bharat Kalluri <bharatkalluri@protonmail.com>
2020-11-24 17:51:33 +0530

    Added a console log

--- pages/api/now-playing.ts
+++ pages/api/now-playing.ts
@@ -1,26 +1,29 @@
-import { getNowPlaying } from '../../lib/spotify';
+import { getNowPlaying } from "../../lib/spotify";
 
 export default async (_: any, res: any) => {
-  const response = await getNowPlaying();
+    const response = await getNowPlaying();
 
-  if (response.status === 204 || response.status > 400) {
-    return res.status(200).json({ isPlaying: false });
-  }
+    if (response.status === 204 || response.status > 400) {
+        console.log(response.status);
+        return res.status(200).json({ isPlaying: false });
+    }
 
-  const song = await response.json();
-  const isPlaying = song.is_playing;
-  const title = song.item.name;
-  const artist = song.item.artists.map((_artist:{name: string}) => _artist.name).join(', ');
-  const album = song.item.album.name;
-  const albumImageUrl = song.item.album.images[0].url;
-  const songUrl = song.item.external_urls.spotify;
+    const song = await response.json();
+    const isPlaying = song.is_playing;
+    const title = song.item.name;
+    const artist = song.item.artists
+        .map((_artist: { name: string }) => _artist.name)
+        .join(", ");
+    const album = song.item.album.name;
+    const albumImageUrl = song.item.album.images[0].url;
+    const songUrl = song.item.external_urls.spotify;
 
-  return res.status(200).json({
-    album,
-    albumImageUrl,
-    artist,
-    isPlaying,
-    songUrl,
-    title
-  });
+    return res.status(200).json({
+        album,
+        albumImageUrl,
+        artist,
+        isPlaying,
+        songUrl,
+        title,
+    });
 };
