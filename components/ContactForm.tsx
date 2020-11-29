239c7560daccde02628d6d3fd072a0d68e86cbd3
Bharat Kalluri <bharatkalluri@protonmail.com>
2020-11-29 17:27:29 +0530

    Updated contact form for formcarry

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,5 +1,5 @@
 import { FormControl, FormLabel } from "@chakra-ui/form-control";
-import React, { useState } from "react";
+import React from "react";
 import { Input } from "@chakra-ui/input";
 import { Button } from "@chakra-ui/button";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
@@ -7,61 +7,37 @@ import { Heading, Stack } from "@chakra-ui/layout";
 import { Textarea } from "@chakra-ui/textarea";
 
 const ContactForm = () => {
-    const [name, setName] = useState<string>();
-    const [email, setEmail] = useState<string>();
-    const [message, setMessage] = useState<string>();
-
     return (
         <>
-            <Stack spacing={5}>
-                <Heading size="xl">Contact me</Heading>
+            <form action="https://formcarry.com/s/OE1SQSGGdn" method="POST">
+                <Stack spacing={5}>
+                    <Heading size="xl">Contact me</Heading>
 
-                <FormControl id="first-name" isRequired>
-                    <FormLabel>Name</FormLabel>
-                    <Input
-                        placeholder="Name"
-                        name="name"
-                        onChange={(e) => setName(e.target.value)}
-                        value={name}
-                    />
-                </FormControl>
+                    <FormControl id="first-name" isRequired>
+                        <FormLabel>Name</FormLabel>
+                        <Input placeholder="Name" name="name" />
+                    </FormControl>
 
-                <FormControl id="email" isRequired>
-                    <FormLabel>Email</FormLabel>
-                    <Input
-                        placeholder="Email"
-                        name="email"
-                        onChange={(e) => setEmail(e.target.value)}
-                        value={email}
-                    />
-                </FormControl>
+                    <FormControl id="email" isRequired>
+                        <FormLabel>Email</FormLabel>
+                        <Input placeholder="Email" name="email" />
+                    </FormControl>
 
-                <FormControl id="message" isRequired>
-                    <FormLabel>Write me a message</FormLabel>
-                    <Textarea
-                        placeholder="Message"
-                        name="message"
-                        onChange={(e) => setMessage(e.target.value)}
-                        value={message}
-                    />
-                </FormControl>
+                    <FormControl id="message" isRequired>
+                        <FormLabel>Write me a message</FormLabel>
+                        <Textarea placeholder="Message" name="message" />
+                    </FormControl>
 
-                <Button
-                    rightIcon={<ArrowForwardIcon />}
-                    colorScheme="teal"
-                    variant="outline"
-                    onClick={() => {
-                        const formData = {
-                            email,
-                            name,
-                            message,
-                        };
-                        console.log(formData);
-                    }}
-                >
-                    Message
-                </Button>
-            </Stack>
+                    <Button
+                        rightIcon={<ArrowForwardIcon />}
+                        colorScheme="teal"
+                        variant="outline"
+                        type="submit"
+                    >
+                        Message
+                    </Button>
+                </Stack>
+            </form>
         </>
     );
 };
