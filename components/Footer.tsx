4e47dada7a658e48e409260bb13f620701bf9e3e
Bharat Kalluri <bharatkalluri@protonmail.com>
2020-12-02 16:26:24 +0530

    Added telegram in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Flex, IconButton, Link } from "@chakra-ui/react";
-import { FiGithub, FiLinkedin, FiMail, FiTwitter } from "react-icons/fi";
+import { FaGithub, FaLinkedin, FaMailBulk, FaTelegramPlane, FaTwitter } from "react-icons/fa";
 import NowPlaying from "./NowPlaying";
 
 
@@ -8,6 +8,7 @@ import NowPlaying from "./NowPlaying";
 const TWITTER_URL = "https://twitter.com/bharatkalluri";
 const GITHUB_URL = "https://github.com/bharatkalluri";
 const LINKEDIN_URL = "https://linkedin.com/bharatkalluri";
+const TELEGRAM_URL = "https://t.me/bharatkalluri";
 const EMAIL = "bharatkalluri@pm.me";
 
 
@@ -18,7 +19,7 @@ const Footer = () => (
             <Link href={TWITTER_URL} title="Twitter" isExternal>
                 <IconButton
                     aria-label="Twitter"
-                    icon={<FiTwitter />}
+                    icon={<FaTwitter />}
                     size="lg"
                     color="gray.500"
                     variant="ghost"
@@ -27,7 +28,7 @@ const Footer = () => (
             <Link href={GITHUB_URL} title="GitHub" isExternal>
                 <IconButton
                     aria-label="GitHub"
-                    icon={<FiGithub />}
+                    icon={<FaGithub />}
                     size="lg"
                     color="gray.500"
                     variant="ghost"
@@ -40,7 +41,20 @@ const Footer = () => (
             >
                 <IconButton
                     aria-label="LinkedIn"
-                    icon={<FiLinkedin />}
+                    icon={<FaLinkedin />}
+                    size="lg"
+                    color="gray.500"
+                    variant="ghost"
+                />
+            </Link>
+            <Link
+                href={TELEGRAM_URL}
+                title="Telegram"
+                isExternal
+            >
+                <IconButton
+                    aria-label="Telegram"
+                    icon={<FaTelegramPlane />}
                     size="lg"
                     color="gray.500"
                     variant="ghost"
@@ -49,7 +63,7 @@ const Footer = () => (
             <Link href={`mailto:${EMAIL}`} title="Email" isExternal>
                 <IconButton
                     aria-label="Email"
-                    icon={<FiMail />}
+                    icon={<FaMailBulk />}
                     size="lg"
                     color="gray.500"
                     variant="ghost"
