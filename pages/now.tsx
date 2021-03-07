0df9cfae070942a35a3af2c3564f188ac347eb65
Bharat Kalluri <bharatkalluri@protonmail.com>
2021-03-07 22:49:30 +0530

    Updated now page

--- pages/now.tsx
+++ pages/now.tsx
@@ -1,21 +1,10 @@
-import {
-    Divider,
-    Link,
-    ListItem,
-    Stack,
-    Text,
-    UnorderedList,
-} from "@chakra-ui/react";
+import { Divider, Link, ListItem, Stack, Text, UnorderedList } from "@chakra-ui/react";
 import NextLink from "next/link";
 import Layout from "../components/Layout";
 
 const Now = () => {
     return (
-        <Layout
-            title="Now"
-            description="Lists down what I am doing currently"
-            relativeCanonicalURL="/now"
-        >
+        <Layout title="Now" description="Lists down what I am doing currently" relativeCanonicalURL="/now">
             <Stack direction="column" spacing={5}>
                 <Text fontSize="6xl" fontWeight="extrabold">
                     Now
@@ -25,32 +14,21 @@ const Now = () => {
                     <ListItem>
                         Working on revamping how people see and manage money at{" "}
                         <Link as="a" textDecoration="underline">
-                            <NextLink href="https://refyne.co.in">
-                                Refyne
-                            </NextLink>
+                            <NextLink href="https://refyne.co.in">Refyne</NextLink>
                         </Link>
                     </ListItem>
-                    <ListItem>Listening to 1984 (audio book)</ListItem>
-                    <ListItem>
-                        Reading thinking fast and slow by daniel kahneman
-                    </ListItem>
+                    <ListItem>Reading thinking fast and slow by daniel kahneman</ListItem>
                     <ListItem>Trying to deeply understand Typescript</ListItem>
-                    <ListItem>Trying to get into fitness</ListItem>
+                    <ListItem>Currently not working on any side projects, exploring what I can do</ListItem>
+                    <ListItem>Trying out time tracking</ListItem>
                     <ListItem>
-                        Currently not working on any side projects, exploring
-                        what I can do
-                    </ListItem>
-                    <ListItem>
-                        Trying to meet and talk to people who are not in
-                        technology, to get different perspectives
+                        Trying to meet and talk to people who are not in technology, to get different perspectives
                     </ListItem>
                 </UnorderedList>
 
                 <Divider />
 
-                <Text fontSize="sm">
-                    I keep updating this page every weekend
-                </Text>
+                <Text fontSize="sm">I keep updating this page every weekend</Text>
             </Stack>
         </Layout>
     );
