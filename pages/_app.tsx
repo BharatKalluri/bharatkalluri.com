2a6805a82b6620adaad447c2e4b2d8d07818d037
Bharat Kalluri <bharatkalluri@protonmail.com>
2021-06-23 18:21:31 +0530

    Upgraded everything!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import { MDXProvider } from "@mdx-js/react";
 import "../styles/main.css";
-import { MDXComponents } from "../components/MDXComponents";
 import { DefaultSeo } from "next-seo";
 import { DEFAULT_SEO_CONFIG } from "../constants";
 
@@ -12,9 +10,7 @@ function App({ Component, pageProps }: AppProps): React.ReactNode {
         <>
             <DefaultSeo {...DEFAULT_SEO_CONFIG} />
             <ChakraProvider>
-                <MDXProvider components={MDXComponents}>
-                    <Component {...pageProps} />
-                </MDXProvider>
+                <Component {...pageProps} />
             </ChakraProvider>
         </>
     );
