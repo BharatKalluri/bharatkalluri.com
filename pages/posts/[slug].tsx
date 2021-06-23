25721138d6f6c0d35ff49208c412739170814542
Bharat Kalluri <bharatkalluri@protonmail.com>
2021-06-23 18:08:17 +0530

    Some more refactoring

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,85 +1,64 @@
-import fs from 'fs';
-import matter from 'gray-matter';
-import { MDXRemote } from 'next-mdx-remote';
-import { serialize } from 'next-mdx-remote/serialize';
-import path from 'path';
-import Layout from '../../components/Layout';
-import { MDXComponents } from '../../components/MDXComponents';
-import { BlogFrontMatter, BlogFrontMatterValidator } from '../../types';
-import { postFilePaths, POSTS_PATH } from '../../utils/mdxUtils';
-import decodeWith from '../../utils/ioTsUtils';
-import { Stack, Text } from '@chakra-ui/react';
-import React from 'react';
+import { MDXRemote } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
+import Layout from "../../components/Layout";
+import { MDXComponents } from "../../components/MDXComponents";
+import { BlogFrontMatter, BlogFrontMatterValidator } from "../../types";
+import { getNoteMetadata, postFilePaths } from "../../utils/mdxUtils";
+import decodeWith from "../../utils/ioTsUtils";
+import { Stack, Text } from "@chakra-ui/react";
+import React from "react";
 
-
-export default function PostPage(props: { source: any, frontMatter: BlogFrontMatter, postFileName: string }) {
-    const frontMatter = decodeWith(BlogFrontMatterValidator)({
-        ...props.frontMatter,
-        __resourcePath: props.postFileName
-    });
+export default function PostPage(props: { source: any; frontMatter: BlogFrontMatter; postFileName: string }) {
+    const frontMatter = decodeWith(BlogFrontMatterValidator)(props.frontMatter);
     const publishedAt = frontMatter.publishedAt;
     const postTitle = frontMatter.title;
     const postDescription = frontMatter.description;
-    const postLink = `${frontMatter.__resourcePath.replace('.mdx', '')}`;
+    const postLink = `${frontMatter.__resourcePath.replace(".mdx", "")}`;
 
     return (
         <>
             <Layout title={postTitle} description={postDescription} relativeCanonicalURL={postLink}>
                 <Stack
-                    as='article'
+                    as="article"
                     spacing={8}
-                    justifyContent='center'
-                    alignItems='flex-start'
-                    m='0 auto 4rem auto'
-                    w='100%'
+                    justifyContent="center"
+                    alignItems="flex-start"
+                    m="0 auto 4rem auto"
+                    w="100%"
                 >
-                    <Text fontSize='6xl' fontWeight='extrabold'>
+                    <Text fontSize="6xl" fontWeight="extrabold">
                         {frontMatter.title}
                     </Text>
-                    <Text color='grey'>Bharat Kalluri / {publishedAt}</Text>
+                    <Text color="grey">Bharat Kalluri / {publishedAt}</Text>
                     <MDXRemote {...props.source} components={MDXComponents} />
-                    <script defer src='https://commento.cloud.bharatkalluri.com/js/commento.js' />
-                    <div id='commento' />
+                    <script defer src="https://commento.cloud.bharatkalluri.com/js/commento.js" />
+                    <div id="commento" />
                 </Stack>
             </Layout>
         </>
     );
 }
 
-export const getStaticProps = async ({ params }: any) => {
-    const postFileName = `${params.slug}.mdx`
-    const postFilePath = path.join(POSTS_PATH, postFileName);
-    const source = fs.readFileSync(postFilePath);
-
-    const { content, data } = matter(source);
-
+export const getStaticProps = async ({ params }: { params: { slug: string } }) => {
+    const postFileName = `${params.slug}.mdx`;
+    const { frontMatter, content } = getNoteMetadata(postFileName);
     const mdxSource = await serialize(content, {
-        mdxOptions: {
-            remarkPlugins: [],
-            rehypePlugins: []
-        },
-        scope: data
+        scope: frontMatter,
     });
 
     return {
         props: {
             source: mdxSource,
-            frontMatter: data,
-            postFilePath: postFilePath,
-            postFileName: postFileName
-        }
+            frontMatter: frontMatter,
+        },
     };
 };
 
 export const getStaticPaths = async () => {
-    const paths = postFilePaths
-        // Remove file extensions for page paths
-        .map((path) => path.replace(/\.mdx?$/, ''))
-        // Map the path into the static paths object required by Next.js
-        .map((slug) => ({ params: { slug } }));
+    const paths = postFilePaths.map((path) => path.replace(/\.mdx?$/, "")).map((slug) => ({ params: { slug } }));
 
     return {
         paths,
-        fallback: false
+        fallback: false,
     };
 };
--- utils/mdxUtils.ts
+++ utils/mdxUtils.ts
@@ -1,19 +1,22 @@
-import fs from 'fs'
-import path from 'path'
-import { BlogFrontMatter } from '../types';
-import matter from 'gray-matter';
+import fs from "fs";
+import path from "path";
+import { BlogFrontMatter } from "../types";
+import matter from "gray-matter";
 
-export const POSTS_PATH = path.join(process.cwd(), 'posts')
+export const POSTS_PATH = path.join(process.cwd(), "posts");
 
-export const postFilePaths = fs
-    .readdirSync(POSTS_PATH)
-    // Only include md(x) files
-    .filter((path) => /\.mdx?$/.test(path))
+export const postFilePaths = fs.readdirSync(POSTS_PATH).filter((path) => /\.mdx?$/.test(path));
+
+export const getNoteMetadata = (filePath: string): { frontMatter: BlogFrontMatter; content: any } => {
+    const source = fs.readFileSync(path.join(POSTS_PATH, filePath));
+    const { data, content } = matter(source);
+    const frontMatter = { ...data, __resourcePath: filePath } as BlogFrontMatter;
+    return {
+        frontMatter: frontMatter,
+        content: content,
+    };
+};
 
 export const getNotesMetadata: () => BlogFrontMatter[] = () => {
-    return postFilePaths.map((filePath): BlogFrontMatter => {
-        const source = fs.readFileSync(path.join(POSTS_PATH, filePath));
-        const { data } = matter(source);
-        return { ...data, __resourcePath: filePath } as BlogFrontMatter;
-    })
-}
+    return postFilePaths.map((filePath): BlogFrontMatter => getNoteMetadata(filePath).frontMatter);
+};
