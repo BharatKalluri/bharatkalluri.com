72f596e51c2310aefc8c1ee54cd0a2d6542b652f
Bharat Kalluri <bharatkalluri@protonmail.com>
2021-11-01 23:26:50 +0530

    Generate rss and sitemap post build

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-            require('./scripts/generate-sitemap');
-            require('./scripts/generate-rss');
-        }
-        return config;
-    }
-};
