b45b60bb9e85114d6a661d48be2dd75e66a172b1
bharatkalluri <bharatkalluri@protonmail.com>
2021-12-12 16:42:24 +0530

    Fix sitemap generation

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -9,9 +9,9 @@ const globby = require('globby');
 
 	const sitemapList = pages
 		.map((page) => {
-			const path = page.replace('pages', '').replace('.js', '').replace('.mdx', '');
+			const path = page.replace('posts/', '').replace('.mdx', '');
 			const route = path === '/index' ? '' : path;
-			return `<url><loc>${BASE_URL}${route}</loc></url>`;
+			return `<url><loc>${BASE_URL}/posts/${route}</loc></url>`;
 		})
 		.join('');
 	const sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${sitemapList}</urlset>`;
