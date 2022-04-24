6e30f072ed8bdfd549bc9664536f096ee8b761b3
Bharat Kalluri <bharatkalluri@protonmail.com>
2022-04-24 20:15:24 +0530

    Added five mistakes in seo post (#58)
    * Added five mistakes in seo post
    
    * Removing redudant statements

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,10 +1,9 @@
-import { Alert, Box, Code, Divider, Heading, Text, Image } from '@chakra-ui/react';
+import { Alert, Box, Code, Divider, Heading, Text, Image, Center } from '@chakra-ui/react';
 import CustomCodeBlock from './CustomCodeBlock';
 import { CustomLink } from './CustomLink';
 import { YoutubeEmbed } from './YoutubeEmbed';
 import React from 'react';
 import AudioVisualizer from './AudioVisualizer';
-import { Flex } from '@chakra-ui/layout';
 
 // TODO: I have no clue how to type this stuff
 export const MDXComponents = {
@@ -44,9 +43,12 @@ export const MDXComponents = {
 	},
 	img: function ImageFn(props: any) {
 		return (
-			<Flex justifyContent={'center'} py={8} w={'100%'}>
-				<Image {...props} alt={props.alt} borderRadius={'5px'} />
-			</Flex>
+			<Center py={4} w={'100%'} flexDirection={'column'}>
+				<Image {...props} alt={props.alt} borderRadius={'5px'} maxWidth={'700px'} />
+				<Text fontSize={'xs'} color="gray.500" pt={2}>
+					{props.alt}
+				</Text>
+			</Center>
 		);
 	},
 	a: CustomLink,
