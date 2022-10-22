6ea384367e8fc0a7f1df4a5720515e44c9ea4789
Bharat Kalluri <bharatkalluri@protonmail.com>
2022-10-22 21:34:21 +0530

    fixing broken links

--- constants.ts
+++ constants.ts
@@ -75,6 +75,6 @@ export const DEFAULT_SEO_CONFIG: DefaultSeoProps = {
 export const RSS_URL = `/feed.xml`;
 export const TWITTER_URL = 'https://twitter.com/bharatkalluri';
 export const GITHUB_URL = 'https://github.com/bharatkalluri';
-export const LINKEDIN_URL = 'https://linkedin.com/bharatkalluri';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/bharatkalluri/';
 export const TELEGRAM_URL = 'https://t.me/bharatkalluri';
 export const EMAIL = 'hi@bharatkalluri.com';
