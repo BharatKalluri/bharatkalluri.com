f027a7b8ff7b289c538074bb1292d7b742d69e42
Bharat Kalluri <bharatkalluri@protonmail.com>
2023-02-05 23:40:53 +0530

    Here goes nothing

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,9 @@
 import React, { ReactNode } from 'react';
-import NextLink from 'next/link';
 import Head from 'next/head';
-import { Box, Flex, Stack } from '@chakra-ui/layout';
-import { IconButton, useColorMode } from '@chakra-ui/react';
-import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { Flex, Stack } from '@chakra-ui/layout';
 import Footer from './Footer';
 import { NextSeo } from 'next-seo';
 import { BASE_URL } from '../constants';
-import { Button } from '@chakra-ui/button';
 import Script from 'next/script';
 
 type LayoutProps = {
@@ -18,11 +14,6 @@ type LayoutProps = {
 	keywords?: string[];
 	heroImage?: string;
 };
-
-interface INavBarButtonProps {
-	text: string;
-	href: string;
-}
 
 const navBarItems: Array<{ href: string; text: string }> = [
 	{
@@ -47,28 +38,19 @@ const navBarItems: Array<{ href: string; text: string }> = [
 	},
 ];
 
-const NavBarButton = (props: INavBarButtonProps) => {
-	return (
-		<NextLink href={props.href} passHref>
-			<Button variant="ghost" fontWeight="400">
-				{props.text}
-			</Button>
-		</NextLink>
-	);
-};
-
 const DesktopLeftNavBar = () => {
 	return (
-		<Box flexDirection="row">
-			{navBarItems.map((item) => (
-				<NavBarButton text={item.text} href={item.href} key={item.href} />
+		<div className="flex items-center justify-between py-4">
+			{navBarItems.map((el) => (
+				<a href={el.href} className="text-md mr-8" key={el.href}>
+					{el.text}
+				</a>
 			))}
-		</Box>
+		</div>
 	);
 };
 
 const NavBar = () => {
-	const { colorMode, toggleColorMode } = useColorMode();
 	return (
 		<Stack
 			flexDirection="row"
@@ -82,15 +64,15 @@ const NavBar = () => {
 			wrap="wrap"
 		>
 			<DesktopLeftNavBar />
-
-			<Box flexDirection="row" mb={'0.2rem'}>
-				<IconButton
-					aria-label="Toggle dark mode"
-					variant="ghost"
-					onClick={toggleColorMode}
-					icon={colorMode == 'dark' ? <SunIcon /> : <MoonIcon />}
-				/>
-			</Box>
+			{/*TODO: uncomment once tailwind migration is complete*/}
+			{/*<Box flexDirection="row" mb={'0.2rem'}>*/}
+			{/*	<IconButton*/}
+			{/*		aria-label="Toggle dark mode"*/}
+			{/*		variant="ghost"*/}
+			{/*		onClick={toggleColorMode}*/}
+			{/*		icon={colorMode == 'dark' ? <SunIcon /> : <MoonIcon />}*/}
+			{/*	/>*/}
+			{/*</Box>*/}
 		</Stack>
 	);
 };
@@ -111,31 +93,33 @@ const Layout = ({ children, title, description, relativeCanonicalURL, keywords, 
 					...heroImageData,
 				}}
 			/>
-			<div>
-				<Head>
-					<title>{`${title} | Bharat Kalluri`}</title>
-					<meta charSet="utf-8" />
-					<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-					{keywords !== undefined && (
-						<meta name="keywords" content={keywords.map((el) => el.replace('-', ' ')).join(', ')} />
-					)}
 
-					<link rel="icon" type="image/png" href={'/static/logo.png'} />
-					<meta httpEquiv="content-language" content="en-gb" />
-				</Head>
-				<Script
-					id={'microsoft-clarity'}
-					type="text/javascript"
-					dangerouslySetInnerHTML={{
-						__html: `
+			<Head>
+				<title>{`${title} | Bharat Kalluri`}</title>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				{keywords !== undefined && (
+					<meta name="keywords" content={keywords.map((el) => el.replace('-', ' ')).join(', ')} />
+				)}
+
+				<link rel="icon" type="image/png" href={'/static/logo.png'} />
+				<meta httpEquiv="content-language" content="en-gb" />
+			</Head>
+			<Script
+				id={'microsoft-clarity'}
+				type="text/javascript"
+				dangerouslySetInnerHTML={{
+					__html: `
 				                (function(c,l,a,r,i,t,y){
 				                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
 				                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
 				                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
 				                })(window, document, "clarity", "script", "4g3tskl7lj");
 				            `,
-					}}
-				/>
+				}}
+			/>
+
+			<div className={'mx-auto px-4 sm:px-6 lg:px-8'}>
 				<header>
 					<NavBar />
 				</header>
