329522e19a616fc20b1f841bbdcb0b228680cc4a
Bharat Kalluri <bharatkalluri@protonmail.com>
2023-02-05 23:42:26 +0530

    added tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx}',
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+
+		// Or if using `src` directory:
+		'./src/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		fontFamily: {
+			sans: ['ui-sans-serif', 'system-ui'],
+			serif: ['ui-serif', 'Georgia'],
+			mono: ['ui-monospace', 'SFMono-Regular'],
+		},
+		extend: {},
+	},
+	plugins: [require('@tailwindcss/typography')],
+};
