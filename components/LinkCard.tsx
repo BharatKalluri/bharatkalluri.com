df418199e0438e6d98335c00e7b6060feb69fc07
Bharat Kalluri <bharatkalluri@protonmail.com>
2023-02-06 17:15:58 +0530

    remove padding for LinkCard.tsx

--- components/LinkCard.tsx
+++ components/LinkCard.tsx
@@ -11,8 +11,8 @@ interface ILinkCardProps {
 const LinkCard = ({ name, description, link }: ILinkCardProps) => {
 	return (
 		<Link href={link}>
-			<figure className="md:flex rounded-xl p-0 border-2">
-				<div className="pt-6 md:p-8 text-center space-y-2">
+			<figure className="rounded-xl p-0 border-2">
+				<div className="p-8 text-center space-y-2">
 					<h2 className={'text-xl font-black text-left'}>{name}</h2>
 					{description && <p className="text-lg text-left font-medium text-slate-700">{description}</p>}
 				</div>
