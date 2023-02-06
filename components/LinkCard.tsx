bb5272e7b5b2dc47c053c122c797c8cc1818413b
Bharat Kalluri <bharatkalluri@protonmail.com>
2023-02-06 17:10:07 +0530

    fix LinkCard.tsx spacing

--- components/LinkCard.tsx
+++ components/LinkCard.tsx
@@ -12,9 +12,9 @@ const LinkCard = ({ name, description, link }: ILinkCardProps) => {
 	return (
 		<Link href={link}>
 			<figure className="md:flex rounded-xl p-0 border-2">
-				<div className="pt-6 md:p-8 text-center md:text-left space-y-2">
-					<h2 className={'text-xl font-black'}>{name}</h2>
-					{description && <p className="text-lg font-medium text-slate-700">{description}</p>}
+				<div className="pt-6 md:p-8 text-center space-y-2">
+					<h2 className={'text-xl font-black text-left'}>{name}</h2>
+					{description && <p className="text-lg text-left font-medium text-slate-700">{description}</p>}
 				</div>
 			</figure>
 		</Link>
