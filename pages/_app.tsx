71e86577c3819fce7956289b26bc7dc97c97fca7
Bharat Kalluri <bharatkalluri@protonmail.com>
2023-05-08 00:41:12 +0530

    remove more chakra UI deps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,14 @@
 import React from 'react';
 import { AppProps } from 'next/app';
-import { ChakraProvider, extendTheme, ThemeConfig } from '@chakra-ui/react';
 import '../styles/main.css';
 import { DefaultSeo } from 'next-seo';
 import { DEFAULT_SEO_CONFIG } from '../constants/constants';
-const themeConfig: ThemeConfig = {
-	initialColorMode: 'light',
-};
-const theme = extendTheme({
-	config: themeConfig,
-	components: {
-		Text: {
-			baseStyle: {
-				fontSize: 'lg',
-			},
-		},
-	},
-});
 
 function App({ Component, pageProps }: AppProps): React.ReactNode {
 	return (
 		<>
 			<DefaultSeo {...DEFAULT_SEO_CONFIG} />
-			<ChakraProvider theme={theme}>
-				<Component {...pageProps} />
-			</ChakraProvider>
+			<Component {...pageProps} />
 		</>
 	);
 }
