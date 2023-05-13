935f6be5277b92ce859baef8e2d48763532c7ed7
Bharat Kalluri <bharatkalluri@protonmail.com>
2023-05-13 17:50:00 +0530

    adding clarity (#67)

--- next.config.js
+++ next.config.js
@@ -2,20 +2,20 @@ const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: securityHeaders,
-      },
-    ];
-  },
+	headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: securityHeaders,
+			},
+		];
+	},
 };
 
 // https://nextjs.org/docs/advanced-features/security-headers
 const ContentSecurityPolicy = `
     default-src 'self' vercel.live;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live www.clarity.ms;
     style-src 'self' 'unsafe-inline';
     img-src * blob: data:;
     media-src 'none';
@@ -24,41 +24,41 @@ const ContentSecurityPolicy = `
 `;
 
 const securityHeaders = [
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, ''),
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  {
-    key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains; preload',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()',
-  },
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+	{
+		key: 'Content-Security-Policy',
+		value: ContentSecurityPolicy.replace(/\n/g, ''),
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+	{
+		key: 'Referrer-Policy',
+		value: 'origin-when-cross-origin',
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+	{
+		key: 'X-Frame-Options',
+		value: 'DENY',
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+	{
+		key: 'X-Content-Type-Options',
+		value: 'nosniff',
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+	{
+		key: 'X-DNS-Prefetch-Control',
+		value: 'on',
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+	{
+		key: 'Strict-Transport-Security',
+		value: 'max-age=31536000; includeSubDomains; preload',
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+	{
+		key: 'Permissions-Policy',
+		value: 'camera=(), microphone=(), geolocation=()',
+	},
 ];
 
 module.exports = withContentlayer(nextConfig);
