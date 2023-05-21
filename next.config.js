a26530cb33575aacef4fca67ac0e89603061ff51
Bharat Kalluri <bharatkalluri@protonmail.com>
2023-05-21 18:00:24 +0530

    set appDir to true

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { withContentlayer } = require('next-contentlayer');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	experimental: {
-		appDir: false,
+		appDir: true,
 	},
 	headers() {
 		return [
