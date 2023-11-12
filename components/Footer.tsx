495d009ddd18b1e3f12210d92d24ebf48212ee6e
Bharat Kalluri <bharatkalluri@protonmail.com>
2023-11-12 20:13:36 +0530

    removed now playing since I'm mostly on Apple Music now-a-days

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { FaGithub, FaLinkedin, FaMailBulk, FaTelegramPlane, FaTwitter, FaRss } from 'react-icons/fa';
 import { EMAIL, GITHUB_URL, LINKEDIN_URL, TELEGRAM_URL, TWITTER_URL, RSS_URL } from '../constants/constants';
 import Link from 'next/link';
-import NowPlaying from './NowPlaying';
 
 const IconButton = (props: { href: string; iconComponent: any }) => {
 	const { href, iconComponent } = props;
@@ -15,8 +14,7 @@ const IconButton = (props: { href: string; iconComponent: any }) => {
 
 const Footer = () => (
 	<div className={'flex items-center flex-col py-12 space-y-6'}>
-		<NowPlaying />
-		<div className={'flex flex-row space-x-4 text-gray-600'}>
+        <div className={'flex flex-row space-x-4 text-gray-600'}>
 			<IconButton href={TWITTER_URL} iconComponent={<FaTwitter />} />
 			<IconButton href={GITHUB_URL} iconComponent={<FaGithub />} />
 			<IconButton href={LINKEDIN_URL} iconComponent={<FaLinkedin />} />
@@ -24,6 +22,7 @@ const Footer = () => (
 			<IconButton href={`mailto:${EMAIL}`} iconComponent={<FaMailBulk />} />
 			<IconButton href={RSS_URL} iconComponent={<FaRss />} />
 		</div>
+        <div className='text-gray-600'>Hand crafted by Bharat Kalluri</div>
 	</div>
 );
 
