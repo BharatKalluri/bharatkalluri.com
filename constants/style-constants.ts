f10d6186a77638ab4208b74e135d11e6e1f7b149
Bharat Kalluri <bharatkalluri@protonmail.com>
2023-12-25 18:26:53 +0530

    move back to prose-base
    Signed-off-by: Bharat Kalluri <bharatkalluri@protonmail.com>

--- constants/style-constants.ts
+++ constants/style-constants.ts
@@ -4,7 +4,7 @@ import { Lora } from 'next/font/google';
 
 export const serifFont = Lora({ subsets: ['latin'], weight: ['400'] });
 
-export const H1Styles: string = clsx('text-2xl font-bold', serifFont.className);
+export const H1Styles: string = clsx('text-3xl font-bold', serifFont.className);
 export const H2Styles: string = clsx('text-md font-bold');
 export const CardListStyles = clsx('flex flex-col space-y-5 py-4');
 export const ButtonStyles = clsx('rounded-md font-medium text-md hover:bg-gray-300 px-3 md:px-4 py-2');
